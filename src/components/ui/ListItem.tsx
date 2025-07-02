0c3b83c3d1047b8b6f39481763c2dc2612378ecb
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-02 22:31:55 +0200

    feat(ListItem): add ListItem component with delete functionality
    - Implemented ListItem component to display a title and manage active state
    - Added onClick and onDelete props for handling item interactions
    - Utilized clsx for conditional class names based on isActive state
    - Integrated Trash2 icon for delete action button

---
+++ src/components/ui/ListItem.tsx
@@ -0,0 +1,41 @@
+import { Trash2 } from "lucide-react";
+import Button from "./Button";
+import clsx from "clsx";
+
+interface ListItemProps {
+  title?: string;
+  isActive?: boolean;
+  onClick?: () => void;
+  onDelete?: () => void;
+}
+
+export default function ListItem({
+  title,
+  isActive,
+  onClick,
+  onDelete,
+  ...props
+}: ListItemProps) {
+  return (
+    <li
+      className={clsx(
+        "flex items-center justify-between overflow-x-hidden rounded-md px-2 py-1 transition duration-150",
+        isActive ? "bg-blue-500 text-white" : "",
+      )}
+      {...props}
+    >
+      <Button
+        className="flex-1 truncate text-left text-xl md:text-base"
+        onClick={onClick}
+      >
+        {title}
+      </Button>
+
+      {isActive && (
+        <Button onClick={onDelete} className="shrink-0 text-white">
+          <Trash2 />
+        </Button>
+      )}
+    </li>
+  );
+}
