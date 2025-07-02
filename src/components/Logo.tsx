53f93bcf98a4fe9d555a2eaeac477bb76bfb0982
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-03 00:50:40 +0200

    feat(Logo): add Logo component with icon and title

---
+++ src/components/Logo.tsx
@@ -0,0 +1,10 @@
+import { Cat } from "lucide-react";
+
+export default function Logo() {
+  return (
+    <div className="flex items-center">
+      <Cat />
+      <h1 className="text-2xl">DoIt</h1>
+    </div>
+  );
+}
