ada251b5dc25ee51d298e12e47845cd21b1ebb47
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-03 01:13:26 +0200

    feat(Header): create Header component for layout with logo and menu button
    - Implemented the Header component to serve as the top layout for the application.
    - Integrated Logo component for branding.
    - Added a Button that dispatches an action to open the side list when clicked.
    - Utilized responsive design with Tailwind CSS for styling.

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,16 @@
+import { openList } from "../../features/openSideList";
+import { useAppDispatch } from "../../store/hooks";
+import Logo from "../Logo";
+import Button from "../ui/Button";
+import { Menu } from "lucide-react";
+export default function Header() {
+  const dispatch = useAppDispatch();
+  return (
+    <header className="flex items-center justify-between overflow-hidden bg-gray-500 px-2 py-3">
+      <Logo />
+      <Button className="md:hidden" onClick={() => dispatch(openList())}>
+        <Menu className="h-10 w-10" />
+      </Button>
+    </header>
+  );
+}
