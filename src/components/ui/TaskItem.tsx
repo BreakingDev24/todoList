d75f3ba9fec571a9990933cd63c737bdb68b6a82
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-03 23:58:04 +0200

    feat(TaskItem): create TaskItem component for displaying individual tasks
    - Implemented TaskItem component to manage the display of individual tasks.
    - Added properties for task text, ID, checked status, and toggle/remove handlers.
    - Utilized tailwind-variants for styling based on task completion status.

---
+++ src/components/ui/TaskItem.tsx
@@ -0,0 +1,48 @@
+import { tv } from "tailwind-variants";
+import Button from "./Button";
+import { Trash } from "lucide-react";
+
+interface TaskItemProps {
+  text: string;
+  id: string;
+  isChecked: boolean;
+  onToggle?: () => void;
+  onRemove?: () => void;
+}
+
+const taskItem = tv({
+  slots: {
+    root: "flex items-center justify-between gap-1.5 overflow-hidden",
+    text: "flex-1 truncate text-xl",
+  },
+
+  variants: {
+    isChecked: {
+      true: {
+        text: "text-gray-400 line-through",
+      },
+      false: {
+        text: "text-gray-900",
+      },
+    },
+  },
+});
+
+export default function TodoItem({
+  text,
+  id,
+  isChecked,
+  onToggle,
+  onRemove,
+}: TaskItemProps) {
+  const { root, text: textSlot } = taskItem({ isChecked });
+  return (
+    <li key={id} className={root()}>
+      <input type="checkbox" onChange={onToggle} checked={isChecked} />
+      <span className={textSlot()}>{text}</span>
+      <Button onClick={onRemove}>
+        <Trash />
+      </Button>
+    </li>
+  );
+}
