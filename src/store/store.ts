000829e5f0a073972a1b182efb6c801c5109666d
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-04 21:27:14 +0200

    feat(store): implement redux-persist for state persistence
    - Combined reducers for todo and sideList into a rootReducer.
    - Configured redux-persist to persist the todo state across sessions.
    - Updated store configuration to use the rootReducer and added middleware for serializable checks.

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,36 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import todoReducer from "../features/todoSlice.ts";
 import sideListReducer from "../features/openSideList.ts";
-export const store = configureStore({
-  reducer: {
-    sideList: sideListReducer,
-    todo: todoReducer,
-  },
+
+const persistConfig = {
+  key: "todo",
+  storage,
+};
+const rootReducer = combineReducers({
+  todo: persistReducer(persistConfig, todoReducer),
+  sideList: sideListReducer,
 });
+
+export const store = configureStore({
+  reducer: rootReducer,
+  devTools: import.meta.env.MODE !== "production",
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "persist/PERSIST",
+          "persist/REHYDRATE",
+          "persist/PAUSE",
+          "persist/FLUSH",
+          "persist/PURGE",
+          "persist/REGISTER",
+        ],
+      },
+    }),
+});
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
