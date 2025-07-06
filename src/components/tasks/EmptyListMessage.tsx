d317a76a9c82ba9f140be8c80a23afbbd2ce920d
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-06 22:27:48 +0200

    feat(EmptyListMessage): add component to display message when list is not found
    - Introduced the EmptyListMessage component to provide user feedback when no lists are available.
    - The component includes a centered message prompting users to select or create a list.

---
+++ src/components/tasks/EmptyListMessage.tsx
@@ -0,0 +1,11 @@
+export function EmptyListMessage() {
+  return (
+    <div className="grid h-full place-content-center">
+      <p>List not found</p>
+      <p>
+        We can't find the list you're looking for. Select one of your lists from
+        the sidebar or create a new one
+      </p>
+    </div>
+  );
+}
