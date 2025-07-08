8172307b57e87b10a77cfde2d0afbfa461bb3ea6
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-07-08 17:27:25 +0200

    chore(App): remove unused imports from App component
    - Removed `useState`, `reactLogo`, and `viteLogo` imports as they are not utilized in the App component.
    - This cleanup helps to reduce the bundle size and improve code readability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/layout/Header";
 import SideList from "./components/layout/SideList";
